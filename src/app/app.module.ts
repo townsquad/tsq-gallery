86c9cf1c41bfd2a7a6b8b25bc4f4a4684e06cc2d
João Ghignatti <joaoghignatti@JV.local>
2019-07-13 21:47:41 -0300

    Import tsq gallery package for test purpose.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {TSqGalleryModule} from '../../projects/ngx-tsq-gallery/src/lib/tsq-gallery.module';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TSqGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
