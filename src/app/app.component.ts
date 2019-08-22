d9d075c9d97c5880e27b876b233b5d391bbb201c
Guilherme Luersen <gluersen@Guilhermes-MacBook-Pro.local>
2019-08-22 14:34:46 -0300

    Update test images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,6 @@ export class AppComponent {
     // tslint:disable-next-line:max-line-length
     {name: 'file_05.png', type: 'image', displayUrl: './assets/images/05.png', downloadUrl: 'https://cdn.vox-cdn.com/thumbor/GNXFfWIVnp-51ld-aYdKv-JzxRI=/0x0:637x355/1820x1213/filters:focal(269x128:369x228):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/51105501/adventuretime.0.png'},
     // tslint:disable-next-line:max-line-length
-    {name: 'file_06.png', type: 'image', displayUrl: './assets/images/06.png', downloadUrl: 'https://cdn.vox-cdn.com/thumbor/GNXFfWIVnp-51ld-aYdKv-JzxRI=/0x0:637x355/1820x1213/filters:focal(269x128:369x228):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/51105501/adventuretime.0.png'},
-    // tslint:disable-next-line:max-line-length
-    {name: 'file_07.png', type: 'image', displayUrl: './assets/images/07.png', downloadUrl: 'https://cdn.vox-cdn.com/thumbor/GNXFfWIVnp-51ld-aYdKv-JzxRI=/0x0:637x355/1820x1213/filters:focal(269x128:369x228):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/51105501/adventuretime.0.png'},
-    // tslint:disable-next-line:max-line-length
-    {name: 'file_08.png', type: 'image', displayUrl: './assets/images/08.png', downloadUrl: 'https://cdn.vox-cdn.com/thumbor/GNXFfWIVnp-51ld-aYdKv-JzxRI=/0x0:637x355/1820x1213/filters:focal(269x128:369x228):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/51105501/adventuretime.0.png'},
-    // tslint:disable-next-line:max-line-length
     {name: 'file_01.pdf', type: 'pdf', displayUrl: './assets/pdfs/01.pdf', downloadUrl: 'https://cdn.vox-cdn.com/thumbor/GNXFfWIVnp-51ld-aYdKv-JzxRI=/0x0:637x355/1820x1213/filters:focal(269x128:369x228):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/51105501/adventuretime.0.png'},
   ];
 
