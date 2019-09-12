e55aa06c86a8ea4767ccef2bdfe15410dc0cc16a
Guilherme Luersen <gluersen@Guilhermes-MacBook-Pro.local>
2019-09-12 14:51:09 -0300

    Change icons to use font-awesome to make it more consistent between browsers

--- projects/tsq-gallery/src/lib/tsq-gallery.module.ts
+++ projects/tsq-gallery/src/lib/tsq-gallery.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {PdfViewerModule} from 'ng2-pdf-viewer';
+import {FontAwesomeModule, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {faCloudDownloadAlt, faSearchMinus, faSearchPlus, faUndoAlt, faRedoAlt, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
 
 import {TSqGalleryComponent} from './components/main-tsq-gallery/main-tsq-gallery.component';
 import {TSqGalleryViewerComponent} from './components/tsq-gallery-viewer/tsq-gallery-viewer.component';
@@ -14,9 +16,14 @@ import {TSqGalleryViewerComponent} from './components/tsq-gallery-viewer/tsq-gal
   imports: [
     CommonModule,
     PdfViewerModule,
+    FontAwesomeModule,
   ],
   exports: [
     TSqGalleryComponent,
   ]
 })
-export class TSqGalleryModule {}
+export class TSqGalleryModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCloudDownloadAlt, faSearchMinus, faSearchPlus, faUndoAlt, faRedoAlt, faTimesCircle);
+  }
+}
